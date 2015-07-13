aebd1e3288e4bea8584ad37072ca86f61320f078
Dermot Daly <dermot.daly@tapadoo.com>
2015-07-13 15:39:06 +0100

    Initial Commit

---
+++ EircodeKit/EircodeKit.h
@@ -0,0 +1,19 @@
+//
+//  EircodeKit.h
+//  EircodeKit
+//
+//  Created by Dermot Daly on 13/07/2015.
+//  Copyright © 2015 Tapadoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EircodeKit.
+FOUNDATION_EXPORT double EircodeKitVersionNumber;
+
+//! Project version string for EircodeKit.
+FOUNDATION_EXPORT const unsigned char EircodeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EircodeKit/PublicHeader.h>
+
+
